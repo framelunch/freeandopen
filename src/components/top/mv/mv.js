a0cd2543a8ed905eef7123ee08fb2b54ce97c635
fl-sekiguchi <sekiguchi@framelunch.jp>
2022-05-12 21:19:06 +0900

    fix: トップスライド挙動変更

--- src/components/top/mv/mv.js
+++ src/components/top/mv/mv.js
@@ -1,7 +1,12 @@
 import $ from 'jquery';
 import notice from 'libraries-frontend-framelunch/js/notice';
+import gsap from 'gsap';
 
 notice.listen('init', () => {
+  const slide = $('.mv__slider .slide');
+  const slideLength = slide.length;
+  let currentSlide = 0;
+
   $('.copy_animation').each((index, element) => {
     $('span', element).each((index2, element2) => {
       const delay = index2 * 50 + index * 500;
@@ -21,16 +26,19 @@ notice.listen('init', () => {
 
   $('.mv__text').addClass('-start');
 
-  // eslint-disable-next-line
-  const swiper = new Swiper('#mvSlider', {
-    effect: 'fade',
-    fadeEffect: {
-      crossFade: true,
-    },
-    speed: 500,
-    allowTouchMove: false,
-    autoplay: {
-      delay: 5000,
-    },
-  });
+  setInterval(() => {
+    slide.eq(currentSlide).addClass('-lastCurrent');
+    slide.eq(currentSlide).removeClass('-current');
+    currentSlide += 1;
+    if (currentSlide >= slideLength) {
+      currentSlide = 0;
+    }
+    slide.eq(currentSlide).addClass('-current');
+    slide.eq(currentSlide).removeClass('-lastCurrent');
+    const targetSlide = slide.eq(currentSlide);
+    gsap.fromTo(targetSlide, { left: '100%' }, { left: 0, duration: 1.5, ease: 'power4.inOut' });
+    gsap.fromTo($('img', targetSlide), { x: '-100%' }, { x: 0, duration: 1.5, ease: 'power4.inOut' });
+  }, 7000);
+
+  slide.eq(currentSlide).addClass('-current');
 });
